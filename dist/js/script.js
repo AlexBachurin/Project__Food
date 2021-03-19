9bdf8318a871d268374f535e45905cab0367d0f8
Alex <alexbachurin91@gmail.com>
2021-03-19 11:26:02 +0300

    Dynamically creating menu items

--- dist/js/script.js
+++ dist/js/script.js
@@ -253,7 +253,46 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  window.addEventListener('scroll', showModalByScroll);
+  window.addEventListener('scroll', showModalByScroll); // Menu
+
+  class Menu {
+    constructor(src, alt, title, descr, price, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+    } //render item on page
+
+
+    render() {
+      const menuItem = document.createElement('div'); //add classes to new element
+      //check if we have any classes, if not set default class
+
+      if (this.classes.length === 0) {
+        this.classes = ['menu__item'];
+      } else {
+        this.classes.forEach(item => menuItem.classList.add(item));
+      }
+
+      menuItem.innerHTML = `<img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>`;
+      const parent = document.querySelector('.menu__field .container');
+      parent.append(menuItem);
+    }
+
+  }
+
+  new Menu('img/tabs/vegy.jpg', "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 229, 'menu__item').render();
+  new Menu('img/tabs/elite.jpg', "elite", 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 550, 'menu__item').render();
+  new Menu('img/tabs/post.jpg', "post", 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ', 430, 'menu__item').render();
 });
 
 /***/ })
